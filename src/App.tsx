640ff7f9718024b21449a17c4d2116dfd44f369f
fluturecode <fluturecode@gmail.com>
2021-12-03 06:35:05 -0500

    added src/common/Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
+import { Main } from './common/Main';
 import { Nav } from "./features/nav";
 
 export const App: FC = () => {
   return (
-    // Overview
-    // Projects
     // Skills
     // Footer
     <>
       <Nav/>
+      <Main/>
     </>
   )
 };
---
+++ src/common/Main.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+
+export const Main:FC = () => {
+  return (
+    <Main>
+      Projects
+      Overview
+    </Main>
+  )
+}
